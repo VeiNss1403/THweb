a0085e81547b21c002917f0edd8ab5c88a91aa25
VeiNss1403 <hoangloc1403@gmail.com>
2023-03-07 15:44:07 +0700

    Updated WEEK 3 Big SCHOOL by LOC

---
+++ BigSchool/BigSchool/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewsModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CourseController()
+        {
+            _dbContext=new ApplicationDbContext();
+        }
+        // GET: Course
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+            };
+            return View(viewModel);
+        }
+    }
+}
